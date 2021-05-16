a78a1ec46ad8f7f189f9fa150812c6810a0e63bc
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2021-05-16 08:26:48 +0200

    add function to create a new class from template files, create .gitignor in project folder, add some text to generate README.md

--- setupProject.sh
+++ setupProject.sh
@@ -11,6 +11,7 @@ fi
 echo "Setting up project ${PROJECT_NAME} ..."
 
 PROJECT_ROOT=~/workspaces/cpp/${PROJECT_NAME}
+PROJECT_ROOT=~/tmp/${PROJECT_NAME}
 
 echo "Project root is ${PROJECT_ROOT}"
 
@@ -34,6 +35,31 @@ function createDirectoryStructure {
   /bin/mkdir -p ${PROJECT_ROOT}/data
   /bin/mkdir -p ${PROJECT_ROOT}/.vscode
 }
+
+# create new class skeleton from template files
+function createNewClass() {
+  local className=${1}
+  local classFileBaseName=${2}
+  local classIncludeGuard=${3}
+  local classSubDirectory=${4}
+  
+  /bin/mkdir -p ${PROJECT_ROOT}/${classSubDirectory}/include
+  /bin/mkdir -p ${PROJECT_ROOT}/${classSubDirectory}/source
+
+  # replace "CNewClass" by ${className}
+  /usr/bin/echo "s/CNewClass/${className}/g" > newClass.sed
+  # replace "new_class" by ${classFileBaseName}
+  /usr/bin/echo "s/new_class/${classFileBaseName}/g" >> newClass.sed
+  # replace "_NEW_CLASS_H_" by ${classIncludeGuard}
+  /usr/bin/echo "s/_NEW_CLASS_H_/${classIncludeGuard}/g" >> newClass.sed
+  
+  /usr/bin/echo "creating source file \"${PROJECT_ROOT}/${classSubDirectory}/source/${classFileBaseName}.cpp\" ..."
+  /usr/bin/sed -f newClass.sed templates/new_class.cpp > ${PROJECT_ROOT}/${classSubDirectory}/source/${classFileBaseName}.cpp
+  /usr/bin/echo "creating header file \"${PROJECT_ROOT}/${classSubDirectory}/include/${classFileBaseName}.h\" ..."
+  /usr/bin/sed -f newClass.sed templates/new_class.h > ${PROJECT_ROOT}/${classSubDirectory}/include/${classFileBaseName}.h
+
+} # function createNewClass()
+
 
 function generateTopLevelCMakeLists {
   # create the files first lines here to get the variables right
@@ -122,7 +148,11 @@ generateSubLevelCMakeLists lib
 
 generateSubLevelCMakeLists test
 
+# createNewClass className classFileBaseName classIncludeGuard classSubDirectory
+createNewClass CAbstractBaseHello abstract_base_hello _ABSTRACT_BASE_HELLO_H_ greeter 
+
 echo "# Project ${PROJECT_NAME}" > ${PROJECT_ROOT}/README.md
+echo "Project ${PROJECT_NAME} was automatigically created by ${0} at $(date "+%Y-%m-%d--%H-%M-%S")" > ${PROJECT_ROOT}/README.md
 
 /bin/cp -pvf templates/main.cpp.template ${PROJECT_ROOT}/bin/source/main.cpp
 /bin/cp -pvf templates/main.h.template ${PROJECT_ROOT}/bin/include/main.h
@@ -140,6 +170,8 @@ echo "# Project ${PROJECT_NAME}" > ${PROJECT_ROOT}/README.md
 if [ "${INIT_GIT_REPO}" == "true" ] ; then
   echo "intializing Git repository in directory  ${PROJECT_ROOT} ..."
   cd ${PROJECT_ROOT}
+  echo "build/" > .gitignore
+  echo ".cache/" >> .gitignore
   git init
   git add .
   git commit -a -m "inital commit of project '${PROJECT_NAME}'"
